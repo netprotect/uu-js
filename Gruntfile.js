d0f354c8591a7896da1b85a2052de6fc681928b5
Babak Sateli <babak@selectivevpn.com>
2014-09-18 21:53:42 -0400

    Use Grunt require-js instead of shell script (taking advantage of watch)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,42 @@
 module.exports = function (grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    
+    jshint: {
+      all: ['src/**/*.js']
+    },
+    
+    requirejs: {
+      compile: {
+        options: {
+          baseUrl: 'src',
+          name: 'main',
+          mainConfigFile: 'src/main.js',
+          out: 'dev/uu.js',
+          optimize: 'none',
+          paths: {
+            requireLab: 'libs/require'
+          },
+          include: 'requireLab'
+        }
+      }
+    },
+        
+    watch: {
+      scripts: {
+        files: ['src/**/*.js'],
+        tasks: ['jshint', 'requirejs']
+      }
+    }
+    
   });
   
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  // Default
+  grunt.registerTask('default', ['jshint', 'requirejs', 'watch']);
+   
 };
--- build/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# $1 = Environment type: dev, prod
-
-# Default to dev if no argument is set
-r.js -o ${1-dev}.js
