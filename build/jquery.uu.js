79c05d35b0fa3bc879f6f8fd629e7a09f7a0bb7d
Roy Hooper <rhooper@toybox.ca>
2018-06-25 19:39:25 -0400

    make adjustments so that a user-supplied email is always passed back in during set ip.  works around problems with cookies not being set.  also adds support for passing reseller_id and ensures the key is sent on all requests.

--- build/jquery.uu.js
+++ build/jquery.uu.js
@@ -11,7 +11,8 @@
       url: 'https://dragon-check.unblock-us.com/get-status.js', // API url
       timeout: 3000, // Milliseconds
       retry: 3, // How many times should retry if timeout
-      key: '' // Affiliate's key
+      key: '', // Affiliate's key
+      reseller_id: '' // Affiliate's id
     };
 
     var plugin = this,
@@ -35,18 +36,33 @@
       //return url.slice(0, cacheIndex) + "c" + Math.floor(( Math.random() * 1000000) + 1) + "." + url.slice(cacheIndex);
     }
 
-    function _getStatus(args) {
-      var call;
-      
+    function _defaultArgs(args) {
+      var merged = {};
       if (plugin.settings.key === '') {
         throw new Error('Missing key');
       } else {
-        $.extend(args, { key: plugin.settings.key });
+        $.extend(merged, { key: plugin.settings.key });
       }
+      if (plugin.settings.reseller_id) {
+        $.extend(merged, { reseller_id: plugin.settings.reseller_id });
+      }
+      if (cache) {
+        $.each(["ip", "account_id", "email"], function(n, idx) {
+          if (cache[idx]) {
+            merged[idx] = cache[idx];
+          }
+        });
+      }
+      $.extend(merged, args);
+      return merged;
+    }
+
+    function _getStatus(args) {
+      var call;
       
       call = $.ajax({
         url: _generateServerUrl(),
-        data: args,
+        data: _defaultArgs(args),
         dataType: "jsonp",
         cache: false,
         timeout: plugin.settings.timeout,
@@ -89,7 +105,7 @@
       
       call = $.ajax({
         url: url,
-        data: args,
+        data: _defaultArgs(args),
         async: false,
         dataType: "jsonp",
         
